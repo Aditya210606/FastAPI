74584b72ca19775281cdf771f8f7a8ad4a8fafc0
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-06-13 13:26:32 +0530

    computed fields

---
+++ computed_fields.py
@@ -0,0 +1,38 @@
+
+
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,computed_field
+from typing import List,Dict,Optional,Annotated
+
+class Patient(BaseModel):
+   name: str
+   email:EmailStr
+   url:AnyUrl
+   age: int
+   weight: float
+   height: float
+   married: bool      
+   allergies: List[str] 
+   contact_details:Dict[str,str]
+
+   @computed_field
+   @property
+   def calculate_bmi(self) -> float:
+       bmi = round(self.weight/(self.height**2),2)
+       return bmi
+
+                                
+def insert_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.married)
+    print(patient.contact_details)
+    print("Data inserted successfully") 
+    print('BMi', patient.calculate_bmi)   
+
+patient_info ={'name': 'aditya','email':'abc@icici.com','url':'http://linkedin.com','age': "67",'weight':44.5,'height':'1.56','married':'false','allergies':['pollen','dust'],'contact_details':{'phone no.':'123456789','emergency':'213'}} 
+
+patient1 = Patient(**patient_info)
+
+insert_patient_data(patient1)
+
+
+
