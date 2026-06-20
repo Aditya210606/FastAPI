e79275411a28ae44858c55ca17b19d391a2c2dbc
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-06-20 14:01:11 +0530

    prediction model created

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field , computed_field
 from typing import Literal,Annotated
+from fastapi.responses import JSONResponse
 import pickle
 import pandas as pd
 
@@ -8,6 +9,16 @@ with open ('model.pkl' , 'rb') as f :  # read binary means rb
     model = pickle.load(f)
 
 app = FastAPI()
+
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
 
 #created a pydantic model for userinput 
 
@@ -39,8 +50,47 @@ class UserInput(BaseModel):
         return "medium"
      else:
         return "low"
-    
+     
 
+    @computed_field
+    @property
+    def age_group(self) -> str :
+     if self.age < 25:
+        return "young"
+     elif self.age < 45:
+        return "adult"
+     elif self.age < 60:
+        return "middle_aged"
+     else :
+      return "senior"
+     
+    @computed_field
+    @property
+    def city_tier(self) -> int :
+     if self.city in tier_1_cities:
+        return 1
+     elif self.city in tier_2_cities:
+        return 2
+     else:
+        return 3
+
+@app.post('/perdict')
+def perdict_premium(data : UserInput):
+
+   input_df = pd.DataFrame([{
+      
+      'bmi' : data.bmi,
+      'age_group' : data.age_group,
+      'lifestyle_risk' : data.lifestyle_risk,
+      'city_tier' : data.city_tier,
+      'income_lpa' : data.income_lpa,
+      'occupation' : data.occupation
+   }])  
+    
+   prediction = model.predict(input_df)[0]
+
+
+   return JSONResponse (status_code=200 , content={'prediction category': prediction})
 
 
 
