574a614cf8609e2d4f74abc8375cd5265e9a17b9
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-06-21 19:14:11 +0530

    changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
 tier_2_cities = [
-    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Jaipur", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
     "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
     "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
     "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
